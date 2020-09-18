a307efdd346070e4fc0e5fd6aa013ae8cf30f578
Rodrigo <burich.developer@gmail.com>
2020-09-18 11:02:47 -0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,24 @@
+// Get reference variables ro the input and label elements
+const calculateButton = document.querySelector('label')
+const shippingWeight = document.querySelector('input')
+
+//create ref var to logo
+//add click event to the logo and log text to the console
+const navLogo = document.querySelector('img')
+navLogo.addEventListener('click', function(){
+    console.log('Nav Logo Clicked')
+})
+
+//create a ref var to the login button
+//add click event to the login button, log you want lo login
+const loginButton = document.querySelector('button')
+loginButton.addEventListener('click', function(){
+    console.log('Login attempt')
+})
+
+
+// Event
+
+calculateButton.addEventListener('click', function(){
+    console.log('Clicked button')
+})
